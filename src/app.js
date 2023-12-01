a96b52a7f7d5f99b342ad68aaba348fb05467061
kimyungyeom <kgk1599@gmail.com>
2023-12-01 18:24:24 +0900

    feat: app.js에 미들웨어 등록 추가

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import UsersRouter from "./routes/users.router.js";
+import logMiddleware from "./middlewares/log.middleware.js";
+import errorHandlingMiddleware from "./middlewares/error-handling.middleware.js";
 
 // port
 const PORT = 3018;
@@ -10,9 +12,11 @@ const PORT = 3018;
 const app = express();
 
 // global middlewares
+app.use(logMiddleware);
 app.use(express.json());
 app.use(cookieParser());
 app.use("/api", [UsersRouter]);
+app.use(errorHandlingMiddleware);
 
 // 서버 구동
 app.listen(PORT, () => {
