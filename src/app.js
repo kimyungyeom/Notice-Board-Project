c577ca57d8598268bac7f274c1e34af190b26d60
kimyungyeom <kgk1599@gmail.com>
2023-12-01 19:04:19 +0900

    feat: app.js 게시글 router 미들웨어 등록 추가

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import UsersRouter from "./routes/users.router.js";
+import PostsRouter from "./routes/posts.router.js";
 import logMiddleware from "./middlewares/log.middleware.js";
 import errorHandlingMiddleware from "./middlewares/error-handling.middleware.js";
 
@@ -15,7 +16,7 @@ const app = express();
 app.use(logMiddleware);
 app.use(express.json());
 app.use(cookieParser());
-app.use("/api", [UsersRouter]);
+app.use("/api", [UsersRouter, PostsRouter]);
 app.use(errorHandlingMiddleware);
 
 // 서버 구동
