88defbbdefaa2c972cf0a8641a15bae7e99c7b0f
kimyungyeom <kgk1599@gmail.com>
2023-12-01 14:16:02 +0900

    feat : 유저라우터 추가
    - 회원가입, 로그인, 내정보조회 기능을 구현한 router를 import
    - 전역 미들웨어에 추가

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 // import
 import express from "express";
 import cookieParser from "cookie-parser";
+import UsersRouter from "./routes/users.router.js";
 
 // port
 const PORT = 3018;
@@ -8,10 +9,10 @@ const PORT = 3018;
 // app.js - global variables
 const app = express();
 
-// middlewares
+// global middlewares
 app.use(express.json());
 app.use(cookieParser());
-app.use("/api", []);
+app.use("/api", [UsersRouter]);
 
 // 서버 구동
 app.listen(PORT, () => {
