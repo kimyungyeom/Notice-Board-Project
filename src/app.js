d0c847528247843a8af21fdbde7b4045cf1ff0d8
kimyungyeom <kgk1599@gmail.com>
2023-12-05 16:54:40 +0900

    feat: 세션 정보를 MySQL에 저장하기 위한 app.js 코드 추가 및 env 작업

--- src/app.js
+++ src/app.js
@@ -2,6 +2,8 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import expressSession from "express-session";
+import expressMySQLSession from "express-mysql-session";
+import dotEnv from "dotenv";
 import UsersRouter from "./routes/users.router.js";
 import PostsRouter from "./routes/posts.router.js";
 import CommentsRouter from "./routes/comments.router.js";
@@ -13,12 +15,39 @@ const PORT = 3018;
 
 // app.js - global variables
 const app = express();
+dotEnv.config();
+
+// MySQLStore
+const MySQLStore = expressMySQLSession(expressSession);
+const sessionStore = new MySQLStore({
+	user: process.env.DATABASE_USERNAME,
+	password: process.env.DATABASE_PASSWORD,
+	host: process.env.DATABASE_HOST,
+	port: process.env.DATABASE_PORT,
+	database: process.env.DATABASE_NAME,
+	// 세션 만료 기간 1일 설정
+	expiration: 1000 * 60 * 60 * 24,
+	// 세션 테이블 자동 생성
+	createDatabaseTable: true,
+});
 
 // global middlewares
 app.use(logMiddleware);
 app.use(express.json());
 app.use(cookieParser());
-app.use(expressSession());
+app.use(
+	expressSession({
+		// 세션 비밀 키
+		secret: process.env.SESSION_SECRET_KEY,
+		resave: false,
+		saveUninitialized: false,
+		store: sessionStore,
+		cookie: {
+			// 쿠키 만료 기간 1일 설정
+			maxAge: 1000 * 60 * 60 * 24,
+		},
+	}),
+);
 app.use("/api", [UsersRouter, PostsRouter, CommentsRouter]);
 app.use(errorHandlingMiddleware);
 
